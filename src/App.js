49321680c055bf5f1e6ba599bb54d93d1c92a0a5
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-02-26 01:00:24 +0600

    recap react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const nayoks = ['Nahid', 'Hassan', 'Bulbul']
   return (
     <div className="App">
       <header className="App-header">
+        <MovieCounter></MovieCounter>
+        {/* <Nayok  name={nayoks[0]}></Nayok>
+        <Nayok  name={nayoks[1]}></Nayok>
+        <Nayok  name={nayoks[2]}></Nayok> */}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
-
+function MovieCounter() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount(count +1);
+return (
+  <div>
+    <button onClick={handleClick}>Add Movie</button>
+    <h3>Number of movies: {count} </h3>
+  </div>
+)
+ }
+function Nayok(props){
+  return (
+    <div>
+      <h1>Ami {props.name} </h1>
+    </div>
+  )
+}
 export default App;
